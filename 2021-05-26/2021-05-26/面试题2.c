0382a83ffd59701b80f5eb33bcb2364d6ecc7643
ChangJunpeng <1135130670@qq.com>
2021-05-26 18:04:40 +0800

    2021-05-26学习代码

--- 2021-05-26/2021-05-26/面试题2.c
+++ 2021-05-26/2021-05-26/面试题2.c
@@ -1,34 +1,34 @@
-#define _CRT_SECURE_NO_WARNINGS 10
-#include<stdio.h>
-
-//删除有序数组中的重复项
-int removeDuplicates(int* nums, int numsSize){
-	if ((numsSize>0) && (numsSize == 0 || numsSize == 1)){
-		return    numsSize;
-	}
-
-	if (numsSize>0){
-		int realArrIndex = 0;
-		int i = 0;
-		for (; i < numsSize; i++){
-			if (nums[realArrIndex] != nums[i]){
-				nums[++realArrIndex] = nums[i];
-			}
-		}
-		return realArrIndex + 1;
-	}
-	return 0;
-	
-}
-
-void test1(){
-	int nums[] = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-	int sz = sizeof(nums) / sizeof(nums[0]);
-	removeDuplicates(nums,-1);
-}
-
-
-int main(){
-	test1();
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 10
+//#include<stdio.h>
+//
+////删除有序数组中的重复项
+//int removeDuplicates(int* nums, int numsSize){
+//	if ((numsSize>0) && (numsSize == 0 || numsSize == 1)){
+//		return    numsSize;
+//	}
+//
+//	if (numsSize>0){
+//		int realArrIndex = 0;
+//		int i = 0;
+//		for (; i < numsSize; i++){
+//			if (nums[realArrIndex] != nums[i]){
+//				nums[++realArrIndex] = nums[i];
+//			}
+//		}
+//		return realArrIndex + 1;
+//	}
+//	return 0;
+//	
+//}
+//
+//void test1(){
+//	int nums[] = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+//	int sz = sizeof(nums) / sizeof(nums[0]);
+//	removeDuplicates(nums,sz);
+//}
+//
+//
+//int main(){
+//	test1();
+//	return 0;
+//}
--- 2021-05-26/2021-05-26/面试题3.c
+++ 2021-05-26/2021-05-26/面试题3.c
@@ -1,60 +1,60 @@
-#define _CRT_SECURE_NO_WARNINGS 10
-
-#include<stdio.h>
-//合并两个有序数组
-void merge(int* nums1, int m, int* nums2,  int n){
-
-	if (n<1){
-		return;
-	}
-	if (m == 0 && n == 1){
-		nums1[0] = nums2[0];
-	}
-
-	if (m + n >= 2){
-
-		int nums1NewEndIndex = m + n - 1;
-		int nums1EndIndex = m - 1;
-		int nums2EndIndex = n - 1;
-
-		int i = 0;
-		while ((nums1NewEndIndex >= 0)){
-
-			if (nums1EndIndex<0){
-				nums1[nums1NewEndIndex--] = nums2[nums2EndIndex--];
-				continue;
-			}
-
-			if (nums2[nums2EndIndex]>nums1[nums1EndIndex]){
-				if (!nums2EndIndex){
-					nums1[nums1NewEndIndex] = nums2[nums2EndIndex];
-					break;
-				}
-				nums1[nums1NewEndIndex--] = nums2[nums2EndIndex--];
-
-			}
-			else {
-				nums1[nums1NewEndIndex--] = nums1[nums1EndIndex--];
-			}
-
-		}
-
-
-	}
-}
-
-
-
-//合并两个有序数组
-/*
-	思路：
-	倒叙排放，将最大的数据放到最后一位，然后一次类推，
-
-*/
-int main(){
-	int nums1[] = { 0,  1, 1, 2,  3, 4 };
-	int nums2[] = { 0,  1, 2, 2, 3, 3, 4 };
-	//	int sz = sizeof(nums) / sizeof(nums[0]);
-	merge(nums1,6,nums2,7);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 10
+//
+//#include<stdio.h>
+////合并两个有序数组
+//void merge(int* nums1, int m, int* nums2,  int n){
+//
+//	if (n<1){
+//		return;
+//	}
+//	if (m == 0 && n == 1){
+//		nums1[0] = nums2[0];
+//	}
+//
+//	if (m + n >= 2){
+//
+//		int nums1NewEndIndex = m + n - 1;
+//		int nums1EndIndex = m - 1;
+//		int nums2EndIndex = n - 1;
+//
+//		int i = 0;
+//		while ((nums1NewEndIndex >= 0)){
+//
+//			if (nums1EndIndex<0){
+//				nums1[nums1NewEndIndex--] = nums2[nums2EndIndex--];
+//				continue;
+//			}
+//
+//			if (nums2[nums2EndIndex]>nums1[nums1EndIndex]){
+//				if (!nums2EndIndex){
+//					nums1[nums1NewEndIndex] = nums2[nums2EndIndex];
+//					break;
+//				}
+//				nums1[nums1NewEndIndex--] = nums2[nums2EndIndex--];
+//
+//			}
+//			else {
+//				nums1[nums1NewEndIndex--] = nums1[nums1EndIndex--];
+//			}
+//
+//		}
+//
+//
+//	}
+//}
+//
+//
+//
+////合并两个有序数组
+///*
+//	思路：
+//	倒叙排放，将最大的数据放到最后一位，然后一次类推，
+//
+//*/
+//int main(){
+//	int nums1[] = { 0,  1, 1, 2,  3, 4 };
+//	int nums2[] = { 0,  1, 2, 2, 3, 3, 4 };
+//	//	int sz = sizeof(nums) / sizeof(nums[0]);
+//	merge(nums1,6,nums2,7);
+//	return 0;
+//}
