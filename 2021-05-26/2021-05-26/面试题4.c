d4c787baff82ed87ce73d6fc6f25d54c4b5b009f
ChangJunpeng <1135130670@qq.com>
2021-05-26 15:29:16 +0800

    旋转数组

---
+++ 2021-05-26/2021-05-26/面试题4.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 10
+#include<stdio.h>
+void reverse(int* nums, int startIndex, int endIndex);
+
+
+
+void rotate(int* nums, int numsSize, int k){
+	
+	//k如果比numSize小，两数求余则是k。
+	//k如果比numSize大，两数这是求余数。
+	k= k%numsSize;
+
+	reverse(nums, 0, numsSize);
+	reverse(nums, 0, k);
+	reverse(nums, k, numsSize);
+
+
+
+}
+
+void reverse(int* nums, int startIndex, int endIndex){
+	// if(endIndex-startIndex == 1)return ;
+	if ((startIndex < endIndex) && (endIndex>0)) {
+
+		int i = startIndex, j = endIndex - 1;
+		for (; i < j; i++, j--){
+			int tmp = nums[i];
+			nums[i] = nums[j];
+			nums[j] = tmp;
+		}
+	}
+
+}
+//旋转数组
+int main(){
+
+	int num[] = { 1, 2, 3, 4, 5, 6, 7 };
+	int k = 3;
+	rotate(num,3,7);
+	return 0;
+}
